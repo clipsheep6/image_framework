a99b09fbcaeae21ae65ff77bdc68a6c5a04dc09a
zewu-feng <fengzewu@huawei.com>
2022-05-25 14:49:47 +0800

    Framework extension feature adding and add webp encoder
    Signed-off-by: zewu-feng <fengzewu@huawei.com>

--- interfaces/innerkits/include/pixel_map.h
+++ interfaces/innerkits/include/pixel_map.h
@@ -70,6 +70,11 @@ public:
     NATIVEEXPORT int32_t GetWidth();
     NATIVEEXPORT int32_t GetHeight();
     NATIVEEXPORT int32_t GetBaseDensity();
+    NATIVEEXPORT void scale(float xAxis, float yAxis);
+    NATIVEEXPORT void translate(float xAxis, float yAxis);
+    NATIVEEXPORT void rotate(float degrees);
+    NATIVEEXPORT void flip(bool xAxis, bool yAxis);
+    NATIVEEXPORT uint32_t crop(const Rect &rect);
     NATIVEEXPORT void GetImageInfo(ImageInfo &imageInfo);
     NATIVEEXPORT PixelFormat GetPixelFormat();
     NATIVEEXPORT ColorSpace GetColorSpace();
