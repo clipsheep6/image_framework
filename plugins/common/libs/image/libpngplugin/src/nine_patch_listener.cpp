e1b6b25c2936bbf1fbf932af46c71d6d21107d2c
xu-rui-w <xurui101@huawei.com>
2022-06-23 11:14:10 +0800

    image告警修改
    Signed-off-by: xu-rui-w <xurui101@huawei.com>

--- plugins/common/libs/image/libpngplugin/src/nine_patch_listener.cpp
+++ plugins/common/libs/image/libpngplugin/src/nine_patch_listener.cpp
@@ -87,7 +87,7 @@ bool NinePatchListener::ReadChunk(const std::string &tag, void *data, size_t len
             return false;
         }
         PngNinePatchRes::Deserialize(patchNew);
-        patchNew->FileToDevice();
+        (PngNinePatchRes *)patchNew->FileToDevice();
         void* patch_ = nullptr;
         if (patch_ != nullptr) {
             free(patch_);
