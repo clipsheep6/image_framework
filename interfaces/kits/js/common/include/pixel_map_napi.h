15c09a138910546eb209bc69013b5222d6c8603f
renhw <renhongwei9@huawei.com>
2023-01-03 03:10:57 +0000

    update interfaces/kits/js/common/include/pixel_map_napi.h.
    Signed-off-by: renhw <renhongwei9@huawei.com>

--- interfaces/kits/js/common/include/pixel_map_napi.h
+++ interfaces/kits/js/common/include/pixel_map_napi.h
@@ -33,6 +33,8 @@ public:
 
     static napi_value CreatePixelMap(napi_env env, std::shared_ptr<PixelMap> pixelmap);
     static std::shared_ptr<PixelMap> GetPixelMap(napi_env env, napi_value pixelmap);
+    static void PixelMapNapi::NativeRelease(napi_env env, napi_value value);
+    static void PixelMapNapi::DeleteReference(napi_env env);
     std::shared_ptr<PixelMap>* GetPixelMap();
     bool IsLockPixelMap();
     bool LockPixelMap();
