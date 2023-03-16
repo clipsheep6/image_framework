fdb4784f791fcec186d42ca3358d04fc6244e5f5
renhw001 <renhongwei9@huawei.com>
2023-03-16 10:03:46 +0000

    update frameworks/innerkitsimpl/utils/include/image_utils.h.
    Signed-off-by: renhw001 <renhongwei9@huawei.com>

--- frameworks/innerkitsimpl/utils/include/image_utils.h
+++ frameworks/innerkitsimpl/utils/include/image_utils.h
@@ -41,6 +41,7 @@ public:
     static MultimediaPlugin::PluginServer& GetPluginServer();
     static bool CheckMulOverflow(int32_t width, int32_t bytesPerPixel);
     static bool CheckMulOverflow(int32_t width, int32_t height, int32_t bytesPerPixel);
+
 private:
     static uint32_t RegisterPluginServer();
 };
