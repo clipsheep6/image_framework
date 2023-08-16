35988da79a08c957ac51845ead6c3f299e5f60e2
genghao <genghao13@huawei.com>
2023-08-16 11:21:25 +0800

    SVG fit canvas size
    Signed-off-by: genghao <genghao13@huawei.com>

--- plugins/common/libs/image/libsvgplugin/src/svg_decoder.cpp
+++ plugins/common/libs/image/libsvgplugin/src/svg_decoder.cpp
@@ -520,18 +520,21 @@ uint32_t SvgDecoder::DoSetDecodeOptions(uint32_t index, const PixelDecodeOptions
         return Media::ERROR;
     }
 
-    if (opts_.desiredSize.width == 0 && opts_.desiredSize.height == 0) {
-        opts_.desiredSize.width = static_cast<uint32_t>(svgSize.width());
-        opts_.desiredSize.height = static_cast<uint32_t>(svgSize.height());
+    float scaleFitDesired = 1.0;
+    if (opts_.desiredSize.width && opts_.desiredSize.height) {
+        scaleFitDesired = std::min(static_cast<float>(opts_.desiredSize.width / svgSize.width()),
+            static_cast<float>(opts_.desiredSize.height / svgSize.height()));
     }
-
-    float scaleFitDesired = std::min(static_cast<float>(opts_.desiredSize.width / svgSize.width()),
-        static_cast<float>(opts_.desiredSize.height / svgSize.height()));
 
     if (opts_.plSVGResize.isValidPercentage) {
         svgDom_->setResizePercentage(static_cast<uint32_t>(opts_.plSVGResize.resizePercentage * scaleFitDesired));
     } else {
         svgDom_->setResizePercentage(static_cast<uint32_t>(DEFAULT_RESIZE_PERCENTAGE * scaleFitDesired));
+    }
+
+    if (!opts_.desiredSize.width && !opts_.desiredSize.height) {
+        opts_.desiredSize.width = static_cast<uint32_t>(svgDom_->containerSize().width());
+        opts_.desiredSize.height = static_cast<uint32_t>(svgDom_->containerSize().height());
     }
 
     info.size.width = opts_.desiredSize.width;
