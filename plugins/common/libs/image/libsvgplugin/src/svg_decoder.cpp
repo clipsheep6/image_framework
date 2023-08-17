e173711bdd0b485faae12c73577a64533aad09a3
genghao <genghao13@huawei.com>
2023-08-17 17:56:52 +0800

    SVG fit canvas size debug
    Signed-off-by: genghao <genghao13@huawei.com>

--- plugins/common/libs/image/libsvgplugin/src/svg_decoder.cpp
+++ plugins/common/libs/image/libsvgplugin/src/svg_decoder.cpp
@@ -517,9 +517,9 @@ uint32_t SvgDecoder::DoSetDecodeOptions(uint32_t index, const PixelDecodeOptions
     }
 
     float scaleFitDesired = 1.0;
-    if (opts_.desiredSize.width && opts_.desiredSize.height) {
-        scaleFitDesired = std::min(static_cast<float>(opts_.desiredSize.width / svgSize.width()),
-            static_cast<float>(opts_.desiredSize.height / svgSize.height()));
+    if (opts_.desiredSize.width > 0 && opts_.desiredSize.height > 0) {
+        scaleFitDesired = std::min(static_cast<float>(opts_.desiredSize.width) / svgSize.width(),
+            static_cast<float>(opts_.desiredSize.height) / svgSize.height());
     }
 
     if (opts_.plSVGResize.isValidPercentage) {
