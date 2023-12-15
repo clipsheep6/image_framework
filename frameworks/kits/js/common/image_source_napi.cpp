f41c897c1921baca46a9e863168f4f0c8ec9eb28
Cao Chuan <caochuan@huawei.com>
2023-12-15 15:13:34 +0800

    Fix code error
    Signed-off-by: Cao Chuan <caochuan@huawei.com>

--- frameworks/kits/js/common/image_source_napi.cpp
+++ frameworks/kits/js/common/image_source_napi.cpp
@@ -610,9 +610,9 @@ static bool ParseColorSpaceInfo(napi_env env, napi_value colorSpace, ColorSpaceI
     if (skColorSpace == nullptr) {
         HiLog::Error(LABEL, "To skcolorspace failed");
         return false;
+    }
     skcms_Matrix3x3 skMatrix;
     skColorSpace->toXYZD50(&skMatrix);
-    }
     for (int i = 0; i < ColorSpaceInfo::XYZ_SIZE; ++i) {
         for (int j = 0; j < ColorSpaceInfo::XYZ_SIZE; ++j) {
             colorSpaceInfo.xyz[i][j] = skMatrix.vals[i][j];
