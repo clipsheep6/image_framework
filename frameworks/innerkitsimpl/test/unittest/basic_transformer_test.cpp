3956a802b687c30678dc693052e93b39f8d09ff8
wangpiao <wangpiao3@h-partners.com>
2024-02-26 03:33:18 +0000

    update frameworks/innerkitsimpl/test/unittest/basic_transformer_test.cpp.
    Signed-off-by: wangpiao <wangpiao3@h-partners.com>

---
+++ frameworks/innerkitsimpl/test/unittest/basic_transformer_test.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define private public
+#include <gtest/gtest.h>
+#include image_type.h
+#include "basic_transformer.h"
+
+using namespace testing::ext;
+using namespace OHOS::Media;
+namespace OHOS {
+namespace Multimedia {
+constexpr int32_t PIXEL_MAP_MAX_RAM_SIZE = 600 * 1024 * 1024;
+class BasicTransformerTest : public testing::Test {
+public:
+    BasicTransformerTest() {}
+    ~BasicTransformerTest() {}
+};
