c44586005c3370c18b0e44061f5ae30e92af4ca6
zhubingwei <zhubingwei@huawei.com>
2024-03-19 10:49:38 +0800

    fix format
    Signed-off-by: zhubingwei <zhubingwei@huawei.com>

--- frameworks/innerkitsimpl/receiver/src/image_receiver.cpp
+++ frameworks/innerkitsimpl/receiver/src/image_receiver.cpp
@@ -282,8 +282,9 @@ OHOS::sptr<OHOS::SurfaceBuffer> ImageReceiver::ReadLastImage()
         bufferBefore = buffer;
         surfaceError = listenerConsumerSurface->AcquireBuffer(buffer, flushFence, timestamp, damage);
     }
-    IMAGE_LOGD("[ImageReceiver] ReadLastImage %{public}lld", static_cast<long long>(timestamp));
+    
     iraContext_->currentBuffer_ = bufferBefore;
+    IMAGE_LOGD("[ImageReceiver] ReadLastImage %{public}lld", static_cast<long long>(timestamp));
     return iraContext_->GetCurrentBuffer();
 }
 
