144ae0b82038c21d57206c29357ea5cc49873847
zhubingwei <zhubingwei@huawei.com>
2024-03-23 09:59:27 +0000

    判断ec是否为空
    Signed-off-by: zhubingwei <zhubingwei@huawei.com>
    Change-Id: I3c5dffe502d56cca29be3b7936a4b0aa7a384ce9

--- frameworks/innerkitsimpl/accessor/src/exif_metadata.cpp
+++ frameworks/innerkitsimpl/accessor/src/exif_metadata.cpp
@@ -162,6 +162,9 @@ int ExifMetadata::GetValue(const std::string &key, std::string &value) const
 
         MnoteHuaweiEntryCount *ec = nullptr;
         mnote_huawei_get_entry_count((ExifMnoteDataHuawei *)md, &ec);
+        if(ec == nullptr) {
+            return ERR_MEDIA_NO_EXIF_DATA;
+        }
         for (unsigned int i = 0; i < ec->size; i++) {
             MnoteHuaweiEntry *entry = ec->entries[i];
             if (key == mnote_huawei_tag_get_name(entry->tag)) {
