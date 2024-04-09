60ed5dde421a118921204d794ea79605e9c1c7da
caoyawen05 <zhubingwei@huawei.com>
2024-04-09 17:40:14 +0800

    修门禁
    Change-Id: Id5d21a2dc475de14dbb4b669ea56b9d4f6f0b594
    Signed-off-by: caoyawen05 <zhubingwei@huawei.com>

--- frameworks/innerkitsimpl/test/unittest/image_source_test/metadata_stream_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/image_source_test/metadata_stream_test.cpp
@@ -1363,14 +1363,14 @@ HWTEST_F(MetadataStreamTest, BufferMetadataStream_Write007, TestSize.Level3)
     BufferMetadataStream stream;
     byte *buf = new byte[13];
     ASSERT_TRUE(stream.Open());
-    stream.Write((uint8_t*)"Hello, world!", 13);
+    stream.Write((uint8_t *)"Hello, world!", 13);
     stream.Seek(4, SeekPos::BEGIN);
-    stream.Write((uint8_t*)"a", 1);
-    stream.Write((uint8_t*)"b", 1);
+    stream.Write((uint8_t *)"a", 1);
+    stream.Write((uint8_t *)"b", 1);
     stream.Seek(0, SeekPos::BEGIN);
     stream.Read(buf, 13);
     ASSERT_STREQ((char *)buf, "Hellab world!");
-    delete []buf;
+    delete[] buf;
 }
 
 HWTEST_F(MetadataStreamTest, BufferMetadataStream_Write008, TestSize.Level3)
@@ -1383,8 +1383,8 @@ HWTEST_F(MetadataStreamTest, BufferMetadataStream_Write008, TestSize.Level3)
     stream.Write(buf2, 500);
     stream.Write(buf2, 500);
     ASSERT_EQ(stream.GetSize(), 3000);
-    delete []buf;
-    delete []buf2;
+    delete[] buf;
+    delete[] buf2;
 }
 
 /**
