45ff6b2ae578533530971973d4bb91c5addb2208
zhubingwei <zhubingwei@huawei.com>
2024-04-15 19:35:14 +0800

    tdd测试代码提交

--- frameworks/innerkitsimpl/test/unittest/image_source_util.h
+++ frameworks/innerkitsimpl/test/unittest/image_source_util.h
@@ -23,8 +23,15 @@
 namespace OHOS {
 namespace ImageSourceUtil {
 int64_t PackImage(const std::string &filePath, std::unique_ptr<OHOS::Media::PixelMap> pixelMap);
+int64_t PackNImage(const std::string &filePath, std::unique_ptr<OHOS::Media::PixelMap>& pixelMap, std::string format);
 int64_t PackImage(std::unique_ptr<OHOS::Media::ImageSource> imageSource);
 bool ReadFileToBuffer(const std::string &filePath, uint8_t *buffer, size_t bufferSize);
+
+int64_t PackDNGImage(const std::string &filePath, std::unique_ptr<OHOS::Media::PixelMap>& pixelMap);
+int64_t PackJPEGImage(const std::string &filePath, std::unique_ptr<OHOS::Media::PixelMap>& pixelMap);
+int64_t PackWEBPImage(const std::string &filePath, std::unique_ptr<OHOS::Media::PixelMap>& pixelMap);
+int64_t PackPNGImage(const std::string &filePath, std::unique_ptr<OHOS::Media::PixelMap>& pixelMap);
+
 }
 }
 #endif // FRAMEWORKS_INNERKITSIMPL_TEST_UNITTEST_IMAGE_SOURCE_UTIL_H
