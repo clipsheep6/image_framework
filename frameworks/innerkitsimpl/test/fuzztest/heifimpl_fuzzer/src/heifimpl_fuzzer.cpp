4bbabbf9617b1ee6f858ad16ce0ef1c149e12cb2
何光耀 <heguangyao1@h-partners.com>
2024-05-30 11:29:27 +0800

    fuzztest add HeifDecodeImpl-1

--- frameworks/innerkitsimpl/test/fuzztest/heifimpl_fuzzer/src/heifimpl_fuzzer.cpp
+++ frameworks/innerkitsimpl/test/fuzztest/heifimpl_fuzzer/src/heifimpl_fuzzer.cpp
@@ -14,38 +14,119 @@
  */
 
 #include "heifimpl_fuzzer.h"
+#define private public
 
 #include <cstdint>
 #include <string>
 #include <unistd.h>
 #include <fcntl.h>
 
-
 #include "image_source.h"
+#include "HeifDecoderImpl.h"
+#include "include/core/SKStream.h"
+#include "buffer_source_stream.h"
+#include "ext_decoder.h"
+
 namespace OHOS {
-void CreateImageSourceByFDEXFuzz(const uint8_t* data, size_t size)
+namespace Media {
+    struct HeifStreamMock : public HeifStream{
+    explicit HeifStreamMock(SKStream* stream) : fStream(stream){}
+    
+    ~HeifStreamMock() override{}
+    
+    size_t read(void* buffer, size_t size) override{
+        return fStream->read(buffer,size);
+    }
+
+    bool rewind() override{
+        return fStream->rewind();
+    }
+
+    bool seek(size_t position){
+        return fStream->seek(position);
+    }
+
+    bool hasLength() const override{
+        return fStream->hasLength();
+    }
+
+    size_t getLength() const override{
+        return fStream->getLength();
+    }
+
+    bool hasPosition() const override{
+        return fStream->hasPosition();
+    }
+
+    size_t getPosition() const override{
+        return fStream->getPosition();
+    }
+
+    private:
+    std::unique_ptr<SKStream> fStream;
+
+    }    
+
+void HeifDecodeImplTest001(ImagePlugin::HeifDecoderImpl *heifdecoderimpl, HeifFrameInfo *frameInfo)
+{
+    size_t frameCount = 0;
+    enum SkHeifColorFormat heifColor = kHeifColorFormat_RGB565;
+    int frameIndex = 0;
+    uint8_t dstBuffer = 0;
+    void *context = nullptr;
+
+    heifdecoderimpl->getSequenceInfo(frameInfo, frameCount);
+    heifdecoderimpl->setOutputColor(heifColor);
+    heifdecoderimpl->decode(frameInfo);
+    heifdecoderimpl->decodeSequence(frameIndex, frameInfo);
+    heifdecoderimpl->setDstBuffer(&dstBuffer, frameCount, context);
+    heifdecoderimpl->getScanline(&dstBuffer);
+    heifdecoderimpl->skipScanlines(frameIndex);
+    heifdecoderimpl->getImageInfo(frameInfo);
+    heifdecoderimpl->decodeGainmap();
+    heifdecoderimpl->setGainmapDstBuffer(&dstBuffer, frameCount);
+    heifdecoderimpl->getGainmapInfo(frameInfo);
+    heifdecoderimpl->getTmapInfo(frameInfo);
+    heifdecoderimpl->getHdrType();
+    heifdecoderimpl->getVividMetadata(std::vector<uint8_t>& uwaInfo, std::vector<uint8_t>& displayInfo,std::vector<uint8_t>& lightInfo);
+    heifdecoderimpl->getISOMetadata(std::vector<uint8_t>& isoMetadata);
+    heifdecoderimpl->getErrMsg(std::string& errMsg);
+    heifdecoderimpl->Reinit(HeifFrameInfo *frameInfo);
+    heifdecoderimpl->InitFrameInfo(HeifFrameInfo *frameInfo, const std::shared_ptr<HeifImage> &image);
+    heifdecoderimpl->GetTileSize(const std::shared_ptr<HeifImage> &image, uint32_t &tileWidth, uint32_t &tileHeight);
+    heifdecoderimpl->SetRowColNum();
+    heifdecoderimpl->ProcessChunkHead(uint8_t *data, size_t len);
+    heifdecoderimpl->DecodeGrids(sptr<SurfaceBuffer> &hwBuffer, bool isGainmap = false);
+    heifdecoderimpl->DecodeIdenImage(sptr<SurfaceBuffer> &hwBuffer, bool isGainmap = false);
+    heifdecoderimpl->DecodeSingleImage(std::shared_ptr<HeifImage> &image, sptr<SurfaceBuffer> &hwBuffer, bool isGainmap = false);
+    heifdecoderimpl->ConvertHwBufferPixelFormat(sptr<SurfaceBuffer> &hwBuffer, bool isGainmap = false);
+    heifdecoderimpl->IsDirectYUVDecode();
+    heifdecoderimpl->SetColorSpaceInfo(HeifFrameInfo* info, const std::shared_ptr<HeifImage>& image);
+    heifdecoderimpl->SetHardwareDecodeErrMsg(const uint32_t width, const uint32_t height);
+}
+
+void HeifImplFuzzTest001(const uint8_t* data, size_t size)
 {
     Media::SourceOptions opts;
-    uint32_t errorCode;
-    uint32_t offset = 0;
-    uint32_t length = 1;
-    std::string pathName = "/tmp/test.jpg";
-    int fd = open(pathName.c_str(), O_RDWR, O_CREAT);
-    if (write(fd, data, size) != static_cast<ssize_t>(size)) {
-        close(fd);
-        return;
-    }
-    auto imagesource = Media::ImageSource::CreateImageSource(fd, offset, length, opts, errorCode);
-    Media ::DecodeOptions dopts;
-    imagesource->CreatePixelMap(dopts, errorCode);
-    close(fd);
+    //HeifDecodeImpl.cpp create/init/fuzztest
+    auto heifdecoder = CreateHeifDecoderImpl();
+    void *obj = dynamic_cast<void*>(heifdecoder);
+    ImagePlugin::HeifDecoderImpl *heifdecoderimpl = static_cast<ImagePlugin::HeifDecoderImpl*>(obj);
+    std::unique_ptr<ImagePlugin::InputDataStream> stream_ = BufferSourceStream::CreateSourceStream(data, size);
+    auto skStream = std::make_shared<ImagePlugin::ExtStream>(stream_.release());
+    HeifFrameInfo heifInfo;
+    heifdecoderimpl->init(new HeifStreamMock(skStream.release()), &heifInfo);
+    HeifDecodeImplTest001(heifdecoderimpl, &heifInfo);
+    
+
 }
+} //namespace media
 } // namespace OHOS
 
 /* Fuzzer entry point */
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     /* Run your code on data */
-    OHOS::CreateImageSourceByFDEXFuzz(data, size);
+    OHOS::Media::HeifImplFuzzTest001(data, size);
     return 0;
 }
