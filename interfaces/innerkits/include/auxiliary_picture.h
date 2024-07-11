9ab424de9d20ab3c60a00f1d120b3b1a82079f84
zhao_na <zhao_na@neusoft.com>
2024-07-11 13:32:05 +0800

    add NATIVEEXPORT for innerapi

--- interfaces/innerkits/include/auxiliary_picture.h
+++ interfaces/innerkits/include/auxiliary_picture.h
@@ -27,21 +27,24 @@ namespace OHOS {
 namespace Media {
 class AuxiliaryPicture: public Parcelable {
 public:
-    static std::unique_ptr<AuxiliaryPicture> Create(std::shared_ptr<PixelMap> &content, AuxiliaryPictureType type, Size size = {0, 0});
-    static std::unique_ptr<AuxiliaryPicture> Create(sptr<SurfaceBuffer> &surfaceBuffer, AuxiliaryPictureType type, Size size = {0, 0});
-    AuxiliaryPictureType GetType();
-    void SetType(AuxiliaryPictureType type);
-    virtual Size GetSize();
-    virtual std::shared_ptr<PixelMap> GetContentPixel();
-    void SetContentPixel(std::shared_ptr<PixelMap> content);
-    uint32_t ReadPixels(const uint64_t &bufferSize, uint8_t *dst);
-    uint32_t WritePixels(const uint8_t *source, const uint64_t &bufferSize);
-    std::shared_ptr<ImageMetadata> GetMetadata(MetadataType type);
-    void SetMetadata(MetadataType type, std::shared_ptr<ImageMetadata> metadata);
-    bool HasMetadata(MetadataType type);
-    virtual bool Marshalling(Parcel &data) const override;
-    static AuxiliaryPicture *Unmarshalling(Parcel &data);
-    static AuxiliaryPicture *Unmarshalling(Parcel &parcel, PIXEL_MAP_ERR &error);
+    AuxiliaryPicture() = default;
+    virtual ~AuxiliaryPicture();
+
+    NATIVEEXPORT static std::unique_ptr<AuxiliaryPicture> Create(std::shared_ptr<PixelMap> &content, AuxiliaryPictureType type, Size size = {0, 0});
+    NATIVEEXPORT static std::unique_ptr<AuxiliaryPicture> Create(sptr<SurfaceBuffer> &surfaceBuffer, AuxiliaryPictureType type, Size size = {0, 0});
+    NATIVEEXPORT AuxiliaryPictureType GetType();
+    NATIVEEXPORT void SetType(AuxiliaryPictureType type);
+    NATIVEEXPORT virtual Size GetSize();
+    NATIVEEXPORT virtual std::shared_ptr<PixelMap> GetContentPixel();
+    NATIVEEXPORT void SetContentPixel(std::shared_ptr<PixelMap> content);
+    NATIVEEXPORT uint32_t ReadPixels(const uint64_t &bufferSize, uint8_t *dst);
+    NATIVEEXPORT uint32_t WritePixels(const uint8_t *source, const uint64_t &bufferSize);
+    NATIVEEXPORT std::shared_ptr<ImageMetadata> GetMetadata(MetadataType type);
+    NATIVEEXPORT void SetMetadata(MetadataType type, std::shared_ptr<ImageMetadata> metadata);
+    NATIVEEXPORT bool HasMetadata(MetadataType type);
+    NATIVEEXPORT virtual bool Marshalling(Parcel &data) const override;
+    NATIVEEXPORT static AuxiliaryPicture *Unmarshalling(Parcel &data);
+    NATIVEEXPORT static AuxiliaryPicture *Unmarshalling(Parcel &parcel, PIXEL_MAP_ERR &error);
 
 protected:
     std::shared_ptr<PixelMap> content_;
